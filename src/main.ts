aafccbdfd9fc05410fe3db02ac53d76b9abbb352
MinhNguyenCR <minh.dox.555@gmail.com>
2025-12-17 14:02:42 +0700

    fix(cors): update CORS origin to use environment variable for frontend URL

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
