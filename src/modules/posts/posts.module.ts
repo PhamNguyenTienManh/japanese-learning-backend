31e59fb21f36feea3f0ed6af744c1db8ca34d2ba
MinhNguyenCR <minh.dox.555@gmail.com>
2026-05-29 00:03:07 +0700

    refactor: remove unused user model and stats method from posts service

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -3,7 +3,6 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Posts, PostSchema } from './schemas/posts.schema';
-import { User, UserSchema } from '../users/schemas/user.schema';
 import { Profile, ProfileSchema } from '../profiles/schemas/profiles.schema';
 import { Comment, CommentSchema } from '../comments/schemas/comments.schema';
 import { UploadModule } from '../upload/upload.module';
@@ -14,7 +13,6 @@ import { UserActivitiesModule } from '../user_activities/user_activities.module'
   imports: [
     MongooseModule.forFeature([
       { name: Posts.name, schema: PostSchema },
-      { name: User.name, schema: UserSchema },
       { name: Profile.name, schema: ProfileSchema },
       { name: Comment.name, schema: CommentSchema },
     ]),
